14f088c77a5d2caf973c851fc6dc711602b76eea
MirzalimovaAisha <AishaMoon>
2024-11-26 19:23:44 +0900

    initial

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TODO list',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:todo_list/screens/list_screen.dart';
+import 'package:todo_list/screens/login_screen.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  Future<bool> checkLogin() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLogin = prefs.getBool('isLogin') ?? false;
+    print("[*] isLogin:" + isLogin.toString());
+    return isLogin;
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(const Duration(seconds: 3), (){
+      moveScreen();
+    });
+  }
+
+  void moveScreen() async{
+    await checkLogin().then((isLogin) {
+      if (isLogin) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const ListScreen()));
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('SplashScreen', style: TextStyle(fontSize: 20),),
+            Text('나만의 일정 관리 : TODO 리스트 앱', style: TextStyle(fontSize: 20),)
+          ],
+        ),
+      ),
+    );
+  }
+}
